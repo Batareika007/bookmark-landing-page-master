9f2c8295ae5862e8587522c17d5ebb3c2a76bc4f
Alexei <50496651+Batareika007@users.noreply.github.com>
2022-11-14 22:47:38 +0300

    JS logic ready
    need some improves on style,
    basic logic and style ready + responsive

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const submit = document.getElementById("submit")
 const email = document.getElementById("email")
 const accordionItems = document.querySelectorAll(".accordion__item")
+const openMenu = document.getElementById("openMenu")
+const closeMenu = document.getElementById("closeMenu")
+const navMenu = document.querySelector(".nav")
+const body = document.querySelector("body")
+const logoHeader = document.querySelector(".header__logo")
+const sliderContainer = document.querySelector(".slider__container")
+const sliderItem = document.querySelectorAll(".slider__link")
 
+// form validation
 submit.addEventListener("click", (event) => {
 	event.preventDefault()
 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
@@ -9,13 +17,58 @@ submit.addEventListener("click", (event) => {
 	} else {
 		email.classList.add("active")
 	}
-	return false
+})
+// accordion
+accordionItems.forEach((element) => {
+	element.addEventListener("click", function () {
+		accordionItems.forEach((el) => el.classList.remove("active"))
+		this.classList.add("active")
+	})
 })
 
-accordionItems.forEach((element) => {
-	element.addEventListener("click", function(){
-		accordionItems.forEach(el =>  el.classList.remove('active') )
-        this.classList.toggle('active')
-        console.log(this)
+// hamburger menu
+
+function showMenu() {
+	navMenu.classList.add("active")
+	closeMenu.style.display = "block"
+	openMenu.style.display = "none"
+	body.classList.add("active")
+	logoHeader.classList.add("active")
+}
+function hideMenu() {
+	navMenu.classList.remove("active")
+	closeMenu.style.display = "none"
+	openMenu.style.display = "block"
+	body.classList.remove("active")
+	logoHeader.classList.remove("active")
+}
+
+document.addEventListener('click', event => {
+    if(event.target !== openMenu){
+        hideMenu()
+    }
+})
+
+openMenu.addEventListener("click", showMenu)
+closeMenu.addEventListener("click", hideMenu)
+
+// slider
+
+sliderItem.forEach((item) => {
+	item.addEventListener("click", function () {
+        sliderItem.forEach(item => item.classList.remove('active'))
+        if(this === document.getElementById('slider1')){
+            sliderContainer.style.justifyContent = 'flex-start'
+            this.classList.add('active')
+        }
+        else if (this === document.getElementById('slider2')){
+            sliderContainer.style.justifyContent = 'center'
+            this.classList.add('active')
+        }
+        else if (this === document.getElementById('slider3')){
+            sliderContainer.style.justifyContent = 'flex-end'
+            this.classList.add('active')
+        }
+        
 	})
 })
